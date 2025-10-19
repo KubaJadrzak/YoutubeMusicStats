5f14b8a8b942a680a2f8ea1ac77e61579608dc35
KubaJadrzak <kuba.jadrzak@gmail.com>
2025-10-19 20:57:00 +0200

    improve processing message

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ const form = document.getElementById('uploadForm');
 uploadBtn.addEventListener('click', () => fileInput.click());
 fileInput.addEventListener('change', () => {
   if (fileInput.files.length > 0) {
-    uploadBtn.textContent = 'Processing...';
+    uploadBtn.innerHTML = 'Processing...<br><small>(This can take even a few minutes)</small>';
     uploadBtn.disabled = true;
     uploadBtn.style.opacity = '0.6';
     form.submit();
