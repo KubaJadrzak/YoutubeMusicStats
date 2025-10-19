a826fb3599a606f2085d4aef1ebc2ebb9fbaed85
KubaJadrzak <kuba.jadrzak@gmail.com>
2025-10-19 20:41:48 +0200

    add processing message

--- public/js/index.js
+++ public/js/index.js
@@ -4,5 +4,10 @@ const form = document.getElementById('uploadForm');
 
 uploadBtn.addEventListener('click', () => fileInput.click());
 fileInput.addEventListener('change', () => {
-  if (fileInput.files.length > 0) form.submit();
+  if (fileInput.files.length > 0) {
+    uploadBtn.textContent = 'Processing...';
+    uploadBtn.disabled = true;
+    uploadBtn.style.opacity = '0.6';
+    form.submit();
+  };
 });
